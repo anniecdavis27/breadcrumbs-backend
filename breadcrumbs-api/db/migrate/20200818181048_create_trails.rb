b606f465874f5a62e4f955564f780a7fb5c771c4
Annie Davis <anniecdavis27@gmail.com>
2020-08-18 14:36:22 -0400

    restarted server because of typo

---
+++ breadcrumbs-api/db/migrate/20200818181048_create_trails.rb
@@ -0,0 +1,12 @@
+class CreateTrails < ActiveRecord::Migration[6.0]
+  def change
+    create_table :trails do |t|
+      t.float :startLat
+      t.float :startLng
+      t.float :endLat
+      t.float :endLng
+
+      t.timestamps
+    end
+  end
+end
---
+++ breadcrumbs-api/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Trail.create([
+  { startLat: 43.96413112471674, startLng: -74.04583503122348, endLat: 43.96406870604346, endLng: -74.04583535479642 }
+])
